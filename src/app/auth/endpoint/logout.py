a7574a09bb5fa8ea09d8abd22fb49d6789f20e2d
CHOJINGHO96 <jo4186@naver.com>
2024-03-27 16:32:53 +0900

    로그아웃추가

--- src/app/auth/endpoint/logout.py
+++ src/app/auth/endpoint/logout.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 
 @router.post("/logout")
